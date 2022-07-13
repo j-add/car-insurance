9d0719ee19216d2afd1d595e9413305a640d143b
Jordan Addis <jordanaddis@Jordans-MBP.lan>
2022-07-13 14:44:59 +0100

    Removed merge artifcats- inlined returns

--- src/Utilities/UserFormUtilities.php
+++ src/Utilities/UserFormUtilities.php
@@ -12,18 +12,11 @@ class UserFormUtilities
     public function sanitiseNameInput(string $input): string
     {
         $input = preg_replace('/[^a-zA-Z \'-]/', '', $input);
-<<<<<<< HEAD
         return self::formatToTitleCase($input);
-=======
-        $input = self::formatToTitleCase($input);
-        return $input;
->>>>>>> origin/master
     }
 
     public function sanitiseValueInput(string $value): float
     {
-        $value = preg_replace('/[^0-9\.]/', '', $value);
-//        preg_match('/\.[0-9]{2}/');
-        return $value;
+        return preg_replace('/[^0-9\.]/', '', $value);
     }
 }
