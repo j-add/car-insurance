22f60c73bb8acaf79e3bd45ebf80347fd7337cd8
Jordan Addis <jordanaddis@Jordans-MBP.lan>
2022-07-13 15:35:19 +0100

    Removed unused nav viewhelper - Nav has its own viewhelper

--- src/ViewHelpers/QuotesViewHelper.php
+++ src/ViewHelpers/QuotesViewHelper.php
@@ -87,16 +87,4 @@ class QuotesViewHelper
         return $result;
     }
 
-    public static function displayNavbar(): string
-    {
-        $result = '';
-        $result .= '<nav>';
-        $result .= '<div class="logo">InsureMe</div>';
-        $result .= '<div class="links">';
-        $result .= '<a href="/">Home</a>';
-        $result .= '<a href="/quotes">All Quotes</a>';
-        $result .= '</div></nav>';
-
-        return $result;
-    }
 }
